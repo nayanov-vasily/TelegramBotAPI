aac6da6d3eb6ff3725ef2594fc14c6ddabdc80ec
Vasily Nayanov <nayanov.vasily@yandex.ru>
2019-05-01 02:19:19 +0300

    the getFile function now returns basic file information with a download link

--- TelegramBotAPI.php
+++ TelegramBotAPI.php
@@ -10,6 +10,11 @@ class TelegramBotAPI
 		$this->assoc = empty($options['assoc']) ? false : $options['assoc'];
 	}
 
+	private function setopt($option, $value)
+	{
+		curl_setopt($this->curl, $option, $value);
+	}
+
 	private function exec()
 	{
 		$response = curl_exec($this->curl);
@@ -91,17 +96,6 @@ class TelegramBotAPI
 
 		curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
 
-		$response = $this->exec();
-
-		$this->curl = \curl_init();
-
-		curl_setopt($this->curl, CURLOPT_URL, "https://api.telegram.org/file/bot$this->token/$response->file_path");
-		curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
-
-		$response = curl_exec($this->curl);
-
-		curl_close($this->curl);
-
-		return $response;
+		return $this->exec();
 	}
 }
